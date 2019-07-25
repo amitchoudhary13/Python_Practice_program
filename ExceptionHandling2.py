693e6d3757732dbbbffe502d95fd79f427d59abf
amitchoudhary13 <cool.rock991@gmail.com>
2019-07-25 13:36:56 +0530

    Use assertion, convert Pound to Kilo grams

--- ExceptionHandling2.py
+++ ExceptionHandling2.py
@@ -3,6 +3,7 @@
 Write a program for converting weight from Pound to Kilo grams.
 a) Use assertion for the negative weight.
 b) Use assertion to weight more than 100 KG
+
 '''
 def PoundToKg(pound):
     try:
